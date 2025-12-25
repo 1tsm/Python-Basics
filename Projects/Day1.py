1587e2499751dd2097e90426d420e3dbae330784
tsm <skjajkdkjshdf@gmail.com>
2025-12-25 20:58:24 +0500

    ilk commit

---
+++ Projects/Day1.py
@@ -0,0 +1,4 @@
+"""
+I am learning Python programming. This is my first day working on Python projects.
+I am excited to explore more about this programming language and build interesting applications.
+"""
