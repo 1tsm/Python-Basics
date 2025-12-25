1f10755a15bfe00dc5e44ff9662f0fb3e1166090
tsm <skjajkdkjshdf@gmail.com>
2025-12-25 21:20:31 +0500

    folder nomi o'zgardi

--- Projects/Day1.py
+++ Projects/Day1.py
@@ -1,4 +1,3 @@
-"""
-I am learning Python programming. This is my first day working on Python projects.
-I am excited to explore more about this programming language and build interesting applications.
-"""
+# Python Basics
+# Day1 [Topic - Intro to Python => Python Output | Comments | Variables | Data Types | Input & Output]
+#
